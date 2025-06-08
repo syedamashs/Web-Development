f752956c3ae5d45a9241b8a86b58f236f49f8d73
SYED AMASH S <syedamashtce@gmail.com>
2025-06-08 22:16:15 +0530

    Create Counter.js

---
+++ JavaScript/Counter.js
@@ -0,0 +1,19 @@
+let x = 0;
+document.getElementById("myh1").textContent = x;
+
+
+document.getElementById("inc").onclick = function (){
+    x=x+1;
+    document.getElementById("myh1").textContent = x;
+}
+
+document.getElementById("res").onclick = function (){
+    x=0;
+    document.getElementById("myh1").textContent = x;
+}
+
+document.getElementById("dec").onclick = function (){
+    x=x-1;
+    document.getElementById("myh1").textContent = x;
+}
+
