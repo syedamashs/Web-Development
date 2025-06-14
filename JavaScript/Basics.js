ec1272a3ab0f11e673dc3ad657582f2c483d6f81
SYED AMASH S <syedamashtce@gmail.com>
2025-06-14 21:13:29 +0530

    Update Basics.js

--- JavaScript/Basics.js
+++ JavaScript/Basics.js
@@ -1,14 +1,14 @@
 //Intro--Variables,getById,Datatypes(conversion),Constant
 
 let x = 'Heading'
-document.getElementById("myh1").textContent = `Its a ${x}`
+document.getElementById("myh1").textContent = `Its a ${x}` //Setting texts in Website!!
 
 
-document.getElementById("sub").onclick = function (){
+document.getElementById("sub").onclick = function (){   //Button Clicking Function!!
     let name = document.getElementById("inp").value
-    let age = Number(document.getElementById("inp2").value)
+    let age = Number(document.getElementById("inp2").value)   //Forms Input!!
     if(name === '' || age == 0){
-        window.alert("Name is empty!!! Or Age is zero !!");
+        window.alert("Name is empty!!! Or Age is zero !!"); //Window Alert Message!!
     }
     else{
         document.getElementById("myp").textContent = `I am ${name} and My Age is ${age}`
