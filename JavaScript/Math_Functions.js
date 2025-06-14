6f38245403f8ae7c48e44f874cba465e8c704b73
SYED AMASH S <syedamashtce@gmail.com>
2025-06-14 21:12:20 +0530

    Create Math_Functions.js

---
+++ JavaScript/Math_Functions.js
@@ -0,0 +1,12 @@
+Math.round(x);
+Math.ceil(x);
+Math.floor(x);
+Math.trunc(x);
+Math.pow(x,y);
+Math.sqrt(x,y);
+Math.log(x);
+Math.sin(x); //All Trigonometric Functions
+Math.abs(x);
+Math.sign(x);
+Math.max(x,y,....);
+Math.min(x,y,....);
