1db0123299b5ba8a0b67eb5e156b8053f8a9e71a
SYED AMASH S <syedamashtce@gmail.com>
2025-06-17 14:42:38 +0530

    Create Arrow Functions.js

---
+++ JavaScript/Arrow Functions.js
@@ -0,0 +1,14 @@
+Efficient Way to Use Functions with Function Expressions 
+
+//Function Expressions
+
+const hello = function(){
+  console.log("Hello");
+}
+hello();
+
+
+//Arrow functions:
+
+const hello = () => console.log("Hello");
+hello();
