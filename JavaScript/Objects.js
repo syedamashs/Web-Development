4a2a723596c1df38b3fd43524821b898d39d2a83
SYED AMASH S <syedamashtce@gmail.com>
2025-06-17 21:11:58 +0530

    Create Objects.js

---
+++ JavaScript/Objects.js
@@ -0,0 +1,11 @@
+const person = {
+  firstname : "Name",
+  lastName: "lastname",
+  age:19,
+  sayHello: function(){console.log("Hello")},
+  sayGoodBye: () => console.log("Good Bye"),
+}
+
+
+//Access
+person.firstname
