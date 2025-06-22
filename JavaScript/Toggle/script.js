d7f2d5e615811f840ccc0038d52a3189e877af08
Amash <syedamash.tce@gmail.com>
2025-06-22 23:09:05 +0530

    Toggling

---
+++ JavaScript/Toggle/script.js
@@ -0,0 +1,11 @@
+function toggleb(){
+    document.body.style.backgroundColor="black";
+    document.body.style.color="white";
+
+}
+
+function togglew(){
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black";
+
+}
