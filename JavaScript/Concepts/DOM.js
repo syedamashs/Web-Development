991c12d1ed343891d779151f0d3313a2020e765e
Amash <syedamash.tce@gmail.com>
2025-06-23 16:24:54 +0530

    Dom Modified

--- JavaScript/Concepts/DOM.js
+++ JavaScript/Concepts/DOM.js
@@ -1,9 +1,15 @@
+//1) Create Element:
+
 const newh1 = document.createElement("h1");
+
+//2) Add Properties and Attributes:
 
 newh1.textContent = "Im Amash";
 newh1.style.backgroundColor = "tomato";
 newh1.style.color = "blue";
 newh1.id = "id1";
+
+//3) Append Elements to DOM:
 
 document.body.append(newh1);
 document.body.prepend(newh1);
@@ -15,3 +21,15 @@ box1.append(newh1);
 
 const box2 = document.getElementById("box2");
 document.body.insertBefore(newh1,box2);
+
+//Or Else we can Use the querySelector All:
+
+const boxes = document.querySelectorAll(".box"); //id name common
+document.body.insertBefore(newh1,boxes[0]);
+document.body.insertBefore(newh1,boxes[1]);
+document.body.insertBefore(newh1,boxes[3]);  //Insert anywhere btw the Boxes by query selector all!!!
+
+//4) Remove HTML:
+
+document.body.removeChild("newh1");
+document.getElementById("box1").removeChild("newh1");
