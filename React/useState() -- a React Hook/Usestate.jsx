6b8c571099c93b85b3500ad1a473cfb0aa2081ea
SYED AMASH S <syedamashtce@gmail.com>
2025-07-21 15:45:03 +0530

    Update Usestate.jsx

--- React/useState() -- a React Hook/Usestate.jsx
+++ React/useState() -- a React Hook/Usestate.jsx
@@ -1,1 +1,30 @@
+//Counter Program using useState() ..
 
+import React , {useState} from 'react';
+
+export default function Usestate(){
+
+    const [counter ,setcounter] = useState(0);
+
+    const dec = () => {
+        setcounter(counter-1);
+    }
+
+    const ress = () =>{
+        setcounter(0);
+    }
+
+    const inc = () =>{
+        setcounter(counter+1);
+    }
+
+
+    return(
+        <div>
+            <p>{counter}</p>
+            <button onClick={dec}>Decrement</button>
+            <button onClick={ress}>Reset</button>
+            <button onClick={inc}>Increment</button>
+        </div>
+    );
+};
